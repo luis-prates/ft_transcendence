f75a8f2da7fe42aad7e36037b67a56aa4097f9c7
Luis Prates <lprates@student.42lisboa.com>
2023-05-21 01:50:41 +0100

    Adds dockerignore, fixes and cleans up docker files

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # generate prisma bindings
 npm run prisma:generate
 
 # deploys the latest prisma schema and build the project
-npm run setup:build:unix
+npm run db:dev:restart:unix
 
 # run the command passed to the docker CMD command
 exec "$@"
