d910486637af2c58d0979033483ed98867bdce73
Luis Prates <lprates@student.42lisboa.com>
2023-05-22 22:00:39 +0100

    small updates to docker repo

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -3,15 +3,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
 
-  @Delete('database')
-  deleteDatabase(): string {
-	return this.appService.deleteDatabase();
-  }
+	@Delete('database')
+	deleteDatabase(): string {
+		return (this.appService.deleteDatabase());
+	}
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+	constructor(private prisma: PrismaService) {}
+	getHello(): string {
+		return 'Hello World!';
+	}
+
+	deleteDatabase(): any {
+		return (this.prisma.cleanDb());
+	}
 }
