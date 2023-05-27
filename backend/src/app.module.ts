5492f8e371a07b081287f2141de4600099c42051
Luís Prates <80156766+luis-prates@users.noreply.github.com>
2023-05-27 12:25:26 +0100

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
-// import { UserModule } from './user/user.module';
-// import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
 	imports: [
-		// AuthModule,
-		// UserModule,
-		// PrismaModule,
+		AuthModule,
+		UserModule,
+		PrismaModule,
 		ConfigModule.forRoot({
 			isGlobal: true,
 		})
