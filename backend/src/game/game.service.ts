3694ac66474f12bc7f863d566ec67d9fef1b12d1
Luis Prates <lprates@student.42lisboa.com>
2023-07-03 21:41:20 +0100

    adds game scoring system to leaderboard

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -15,6 +15,7 @@ type LeaderBoard = {
 	userId: number;
 	nickname: string;
 	image: string;
+	points: number;
 	gamesWon: number;
 	gamesLost: number;
 };
@@ -174,10 +175,19 @@ export class GameService {
 		let rank = 1;
 		let prevGamesWon: number = null;
 		let prevGamesLost: number = null;
-		(await leaderboard).forEach(game => {
-			const gamesWon = game.wonGames.length;
-			const gamesLost = game.lostGames.length;
+		(await leaderboard).forEach(user => {
+			const gamesWon = user.wonGames.length;
+			const gamesLost = user.lostGames.length;
 			let currentRank = rank;
+
+			let points = 0;
+
+			user.wonGames.forEach(wongame => {
+				points += wongame.winnerScore * (wongame.loserScore == 0 ? 150 : 100);
+			});
+			user.lostGames.forEach(lostgame => {
+				points += lostgame.loserScore == 0 ? 10 : lostgame.loserScore * 20;
+			});
 
 			if (prevGamesWon !== null && prevGamesLost !== null) {
 				if (gamesWon === prevGamesWon && gamesLost === prevGamesLost) {
@@ -189,9 +199,10 @@ export class GameService {
 
 			leaderboardReturn.push({
 				rank: currentRank,
-				userId: game.id,
-				nickname: game.nickname,
-				image: game.image,
+				userId: user.id,
+				nickname: user.nickname,
+				image: user.image,
+				points: points,
 				gamesWon,
 				gamesLost,
 			});
